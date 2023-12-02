9cd2c32206078b0e5bf0c622e28722d8f83d9071
Michael Schubart <michael@schubart.net>
2023-12-02 09:23:04 +0000

    Factor out parsing and maximum

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -6,31 +6,16 @@ use std::collections::HashMap;
 fn solve1() -> usize {
     include_str!("input.txt")
         .lines()
-        .enumerate()
-        .filter_map(|(index, line)| {
-            let mut parts = line.split(&[':', ',', ';', ' '][..]);
-            parts.next(); // Ignore "Game".
-            parts.next(); // Ignore ID.
+        .map(max_counts)
+        .filter_map(|(id, samples)| {
+            let plausible = samples.iter().all(|(&color, &count)| match color {
+                "red" => count <= 12,
+                "green" => count <= 13,
+                "blue" => count <= 14,
+                _ => true,
+            });
 
-            let mut result = Some(index + 1); // Cheat: Assume ID = line number, don't parse ID.
-
-            while parts.next().is_some() {
-                let count = parts.next().unwrap().parse::<usize>().unwrap();
-                let color = parts.next().unwrap();
-
-                let possible = match color {
-                    "red" => count <= 12,
-                    "green" => count <= 13,
-                    "blue" => count <= 14,
-                    _ => unreachable!(),
-                };
-
-                if !possible {
-                    result = None;
-                }
-            }
-
-            result
+            plausible.then_some(id)
         })
         .sum()
 }
@@ -38,24 +23,28 @@ fn solve1() -> usize {
 fn solve2() -> usize {
     include_str!("input.txt")
         .lines()
-        .map(|line| {
-            let mut parts = line.split(&[':', ',', ';', ' '][..]);
-            parts.next(); // Ignore "Game".
-            parts.next(); // Ignore ID.
-
-            let mut mins = HashMap::new();
-
-            while parts.next().is_some() {
-                let count = parts.next().unwrap().parse::<usize>().unwrap();
-                let color = parts.next().unwrap();
-
-                let min = mins.entry(color).or_default();
-                *min = max(count, *min);
-            }
-
-            mins.values().product::<usize>()
-        })
+        .map(max_counts)
+        .map(|(_, samples)| samples.values().product::<usize>())
         .sum()
+}
+
+// Parse a line, return the game ID and the maximum count of each color seen in the game.
+fn max_counts(line: &str) -> (usize, HashMap<&str, usize>) {
+    let mut parts = line.split(&[':', ',', ';', ' '][..]);
+    parts.next(); // Ignore "Game".
+    let id = parts.next().unwrap().parse().unwrap();
+
+    let mut samples = HashMap::new();
+
+    while parts.next().is_some() {
+        let count = parts.next().unwrap().parse::<usize>().unwrap();
+        let color = parts.next().unwrap();
+
+        let max_count = samples.entry(color).or_default();
+        *max_count = max(count, *max_count);
+    }
+
+    (id, samples)
 }
 
 #[test]
